c650e21365cb92e03b419a8f7db082c3d610bed7
sulguk <sj-nv20@naver.com>
2023-08-31 11:32:08 +0900

    home edit

--- inkwang_metal/src/routes/home.js
+++ inkwang_metal/src/routes/home.js
@@ -2,11 +2,47 @@ import React from 'react';
 import '../scss/_header.scss'
 import '../scss/_footer.scss'
 import '../scss/_home.scss'
+import "swiper/swiper.scss";
+
+import h_img1 from '../images/inkwang_img/inkwang_7.jpeg';
+import h_img2 from '../images/inkwang_img/inkwang_5.jpeg';
+import h_img3 from '../images/inkwang_img/inkwang_4.jpeg';
+import h_img4 from '../images/inkwang_img/inkwang_2.jpeg';
+
+import swiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Home = () => {
+  swiperCore.use([Autoplay]);
   return (
-    <div className="content">
-      홈 화면 입니다.
+    <div className='content'>
+      <Swiper
+        className='swiper-container'
+        effect={'slide'}
+        loop={true}
+        speed={1200}
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 8000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <img src={h_img1} alt="h_img1"/>
+          <div className='img_text'>
+            <h1>인산염피막, 알카리착색 전문<br/>인광금속</h1>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={h_img2} alt="h_img2"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={h_img3} alt="h_img3"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={h_img4} alt="h_img4"/>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
