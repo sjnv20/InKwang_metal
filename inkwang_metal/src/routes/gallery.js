f7be5c1e30370ceafb41fabc43bc45887159048e
sulguk <sj-nv20@naver.com>
2023-09-25 15:37:27 +0900

    page_ing edit

--- inkwang_metal/src/routes/gallery.js
+++ inkwang_metal/src/routes/gallery.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import '../scss/_header.scss'
+import '../scss/_footer.scss'
+import '../scss/_page_ing.scss'
 
 const Gallery = () => {
   return (
-    <div>
-     Gallery 화면 입니다.
+    <div className='page'>
+      <div className='display'>
+        <div className='container'>
+          <h3>페이지 준비중</h3>
+          <div id='spc'>[ Gallery ]</div>
+          <div id='form'>
+            현재 페이지 준비중입니다.<br/>
+          </div>
+          <div id='spc'></div>
+          <a id='home_btn' href='/'>Home</a>
+        </div>
+      </div>
     </div>
   );
 };
