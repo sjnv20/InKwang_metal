5cee0dcb863d64a8bee01b6a6c48f9ecd9679b55
sulguk <sj-nv20@naver.com>
2023-10-05 21:16:22 +0900

    pagination add

--- inkwang_metal/src/routes/home.js
+++ inkwang_metal/src/routes/home.js
@@ -3,6 +3,8 @@ import '../scss/_home_header.scss'
 import '../scss/_footer.scss'
 import '../scss/_home.scss'
 import "swiper/swiper.scss";
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/navigation/navigation.scss';
 
 import Logo from '../images/inkwang_logo.png';
 import h_img1 from '../images/inkwang_img/inkwang_1.jpeg';
@@ -10,12 +12,14 @@ import h_img2 from '../images/inkwang_img/inkwang_img01.jpeg';
 import h_img3 from '../images/inkwang_img/inkwang_img07.jpeg';
 import h_img4 from '../images/inkwang_img/inkwang_img02.jpeg';
 
+import { Parallax, Pagination } from 'swiper';
 import swiperCore, { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+
 const Home = () => {
   
-  swiperCore.use([Autoplay]);
+  swiperCore.use([Autoplay, Pagination ]);
   return (
   <div className='content'>
     <header className="h_navigation">
@@ -37,18 +41,20 @@ const Home = () => {
         effect={'slide'}
         loop={true}
         speed={1200}
-        modules={[Autoplay]}
+        parallax={true}
+        modules={[Parallax, Autoplay, Pagination]}
         autoplay={{
           delay: 4000,
           disableOnInteraction: false,
         }}
+        pagination={{ clickable: true }}
         style={{height:"100vh"}}
       >
         <SwiperSlide>
           <div className='swiper_img1'>
-            <img className='img1' src={h_img1} alt="h_img1"/>
+            <img className='img1' src={h_img1} alt="h_img1" data-swiper-parallax="-23%"/>
             <div className='img_text'>
-              <h1>인산염피막, 알카리착색 전문<br/><span><span id='sp'>|</span> 인광금속</span></h1>
+              <h1 data-swiper-parallax="-300">인산염피막, 알카리착색 전문<br/><span><span id='sp'>|</span> 인광금속</span></h1>
             </div>
           </div>
         </SwiperSlide>
@@ -77,6 +83,7 @@ const Home = () => {
         </div>
         </SwiperSlide>
       </Swiper>
+       
     </div>
   );
 };
