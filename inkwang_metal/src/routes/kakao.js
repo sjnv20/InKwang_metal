497be69fd6226e7f2a428e981f175789678aaae3
sulguk <sj-nv20@naver.com>
2023-10-18 16:20:15 +0900

    kakaomap edit

--- inkwang_metal/src/routes/kakao.js
+++ inkwang_metal/src/routes/kakao.js
@@ -15,7 +15,7 @@ function Kakao() {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3
     };
-    const map = new kakao.maps.Map(container, options);
+    const map = new kakao.maps.Map(container, options); // eslint-disable-line no-unused-vars
     }, [])
 
     return (
