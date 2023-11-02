d5402cc1a50d5e30664c04b1c17f7882ec52e32b
sulguk <sj-nv20@naver.com>
2023-11-02 18:17:24 +0900

    product edit

--- inkwang_metal/src/routes/product.js
+++ inkwang_metal/src/routes/product.js
@@ -109,32 +109,33 @@ const Product = () => {
         </div>
         <div className='product'>
           <div className="box">
-            <Box sx={{ bgcolor: 'background', width: 880, height:400 }}>
-              <AppBar position="static">
+            <Box sx={{ width: 870 }}>
+              <AppBar position="static" style={{backgroundColor:'#fff',  boxShadow:"none"}}>
                 <Tabs
                   value={value}
                   onChange={handleChange}
                   indicatorColor="gray"
                   textColor="inherit"
-                  variant="fullWidth"
+                  variant="scrollable"
                   aria-label="full width tabs example"
+                  sx={{ borderBottom: 1, borderColor: '#d4d4d4'}}
                 >
-                  <Tab sx={{backgroundColor:"#15137C", fontSize:"18px", fontFamily:"NanumSquareBold"}} label="인산염 피막" {...a11yProps(0)} />
-                  <Tab sx={{backgroundColor:"#15137C", fontSize:"18px", fontFamily:"NanumSquareBold"}} label="상세 제품" {...a11yProps(1)} />
+                  <Tab id="p_tab1" sx={{backgroundColor:"#15137C", fontSize:"18px", fontFamily:"NanumSquareBold", width:'20%', borderStartEndRadius:'80px 80px;', opacity:'1', zIndex:"20"}} label="알카리 착색" {...a11yProps(0)} />
+                  <Tab id="p_tab2" sx={{backgroundColor:"#7e7ccf", fontSize:"18px", fontFamily:"NanumSquareBold", width:'20%', borderStartEndRadius:'80px 80px;', left:'-5%', zIndex:"10"}} label="상세 제품" {...a11yProps(1)} />
                 </Tabs>
               </AppBar>
                 <TabPanel value={value} index={0} dir={theme.direction}>
                   <div className='pr_fill'>
                     <img className="pr_img" src={P_product} alt='/'/>
                     <pre>
-                      | 특징 : <br/>
-                      | 표준조건 : <br/>
-                      | 용도 :
+                      - 목적 : 금속 제품을 화학약품 탱크에 담그어 피막을 형성시켜, 금속 제품의 부식을 방지하고 녹을 방지하는 표면 처리 과정 <br/>
+                      - 특징 : <div id='pr_div'>• 무광<br/>• 망간계에 비해 색이 엷음<br/>• 미세두께 오름<br/>• 내식성이 있음<br/></div>
+                      - 용도 : 도장하지용 / 자동차부품 / 산업기계 / 선박용품 등
                     </pre>
                   </div>
                 </TabPanel>
                 <TabPanel value={value} index={1} dir={theme.direction}>
-                  <div className='pr_fill2'>
+                  <div className='pr_fill2' style={{flexFlow:"column wrap"}}>
                     <img className="pm_4" src={pm_4} alt='/'/>
                     <img className="pm_2" src={pm_2} alt='/'/>
                     <img className="pm_1" src={pm_1} alt='/'/>
--- inkwang_metal/src/routes/product_r.js
+++ inkwang_metal/src/routes/product_r.js
@@ -6,6 +6,7 @@ import '../scss/_product.scss'
 import R_product from '../images/inkwang_img/r_product.jpeg'
 import rc_1 from '../images/rc1.jpeg'
 import rc_2 from '../images/rc2.jpeg'
+import rc_3 from '../images/rc3.jpeg'
 
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
@@ -107,27 +108,28 @@ const Product = () => {
         </div>
         <div className='product'>
           <div className="box">
-            <Box sx={{ bgcolor: 'background', width: 880 }}>
-              <AppBar position="static">
+            <Box sx={{ width: 870 }}>
+              <AppBar position="static" style={{backgroundColor:'#fff',  boxShadow:"none"}}>
                 <Tabs
                   value={value}
                   onChange={handleChange}
                   indicatorColor="gray"
                   textColor="inherit"
-                  variant="fullWidth"
+                  variant="scrollable"
                   aria-label="full width tabs example"
+                  sx={{ borderBottom: 1, borderColor: '#d4d4d4'}}
                 >
-                  <Tab sx={{backgroundColor:"#15137C", fontSize:"18px", fontFamily:"NanumSquareBold"}} label="알카리 착색" {...a11yProps(0)} />
-                  <Tab sx={{backgroundColor:"#15137C", fontSize:"18px", fontFamily:"NanumSquareBold"}} label="상세 제품" {...a11yProps(1)} />
+                  <Tab id="p_tab1" sx={{backgroundColor:"#15137C", fontSize:"18px", fontFamily:"NanumSquareBold", width:'20%', borderStartEndRadius:'80px 80px;', opacity:'1', zIndex:"20"}} label="알카리 착색" {...a11yProps(0)} />
+                  <Tab id="p_tab2" sx={{backgroundColor:"#7e7ccf", fontSize:"18px", fontFamily:"NanumSquareBold", width:'20%', borderStartEndRadius:'80px 80px;', left:'-5%', zIndex:"10"}} label="상세 제품" {...a11yProps(1)} />
                 </Tabs>
               </AppBar>
                 <TabPanel value={value} index={0} dir={theme.direction}>
                   <div className='pr_fill'>
                     <img className="pr_img_r" src={R_product} alt='/'/>
                     <pre>
-                      | 특징 : <br/>
-                      | 표준조건 : <br/>
-                      | 용도 :
+                      - 목적 :  금속 제품을 화학 약품 탱크에 일정 시간 동안 담그어 표면 처리하여 부식을 방지하고 쉽게 부식되지 않도록 하는 과정<br/>
+                      - 특징 : <div id='pr_div'>• 유광<br/>• 흑색  -  소재의 특성에 따라 색이 차이는 있음<br/>• 두께 변화 없음  -  정밀가공품에 사용<br/>• 내식성이 약하다</div>
+                      - 용도 :  자동차부품 / 산업기계 / 정밀가공품 등
                     </pre>
                   </div>
                 </TabPanel>
@@ -135,6 +137,7 @@ const Product = () => {
                   <div className='pr_fill2'>
                     <img className="rc_1" src={rc_1} alt='/'/>
                     <img className="rc_2" src={rc_2} alt='/'/>
+                    <img className="rc_3" src={rc_3} alt='/'/>
                   </div>
                 </TabPanel>
             </Box>
